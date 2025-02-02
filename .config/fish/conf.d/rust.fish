4bb701794b9d7ab15638dc4d99d1a647b6311ecb
Nurah Wolfo <nurah@wolfo.tech>
2025-02-02 02:49:22 +0000

    FIX(rust): Did not export RUSTFLAGS

--- .config/fish/conf.d/rust.fish
+++ .config/fish/conf.d/rust.fish
@@ -4,4 +4,4 @@
 #│ 0.0.1        │ 2024-02-10      │ Git     │
 #└──────────────┴─────────────────┴─────────┘
 
-set -U RUSTFLAGS '-C target-cpu=native' # Compile to native code, this enables things like SSE!
+set -x RUSTFLAGS '-C target-cpu=native' # Compile to native code, this enables things like SSE!
